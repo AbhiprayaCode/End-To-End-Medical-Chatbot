88c3dc5ef7869330405b8e457819780ed14733ec
AlifAbhipraya <alifelangabhipraya@gmail.com>
2024-11-11 16:34:25 +0700

    update app.py

--- app.py
+++ app.py
@@ -86,5 +86,8 @@ def chat():
     print("Response:", response["text"])
     return str(response["text"])
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(debug=True)
