6bef68b0ef52e5af63bf8fe72367acf4f5599e06
AbhiprayaCode <109324938+AbhiprayaCode@users.noreply.github.com>
2024-11-29 20:21:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-llm = ChatGroq(
+llm = Groq(
     model="gemma-7b-it",
     temperature=1,
     max_tokens=1024,
